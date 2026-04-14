dd64f391ec56f58c73d8738828e03942f73b08fd
Navied-dipu <navieddipu08@gmail.com>
2026-04-14 14:14:26 +0600

    add navber

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,24 +1,74 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ImStatsDots } from "react-icons/im";
+import { IoHomeOutline } from "react-icons/io5";
+import { RiTimeLine } from "react-icons/ri";
 
 export default function Navber() {
-    const links= <>
-    <li><a>Link</a></li>
-    <li><a>Link</a></li>
-    <li><a>Link</a></li>
+  const pathname = usePathname();
+
+  const links = (
+    <>
+      <li>
+        <Link
+          href="/"
+          className={`flex items-center gap-2 font-semibold ${
+            pathname === "/" ? "bg-[#244d3f] text-white rounded" : ""
+          }`}
+        >
+          <IoHomeOutline /> Home
+        </Link>
+      </li>
+
+      <li>
+        <Link
+          href="/timeline"
+          className={`flex items-center gap-2 font-semibold ${
+            pathname === "/timeline"
+              ? "bg-[#244d3f] text-white rounded"
+              : ""
+          }`}
+        >
+          <RiTimeLine /> Timeline
+        </Link>
+      </li>
+
+      <li>
+        <Link
+          href="/stats"
+          className={`flex items-center gap-2 font-semibold  ${
+            pathname === "/stats"
+              ? "bg-[#244d3f] text-white rounded"
+              : ""
+          }`}
+        >
+          <ImStatsDots /> Stats
+        </Link>
+      </li>
     </>
-    return (
-        <div>
-            <div className="navbar max-w-6xl mx-auto bg-white text-black ">
-                <div className="flex-1">
-                    <Image src="/assets/logo.png" alt="Logo" width={100} height={80}  />
-                </div>
-                <div className="flex-none">
-                    <ul className="menu menu-horizontal px-1">
-                     {links}
-                    </ul>
-                </div>
-            </div>
+  );
+
+  return (
+    <div>
+      <div className="navbar max-w-6xl mx-auto bg-white text-black">
+        <div className="flex-1">
+          <Link href="/">
+            <Image
+              src="/assets/logo.png"
+              alt="Logo"
+              width={100}
+              height={80}
+            />
+          </Link>
         </div>
-    )
+
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+      </div>
+    </div>
+  );
 }
